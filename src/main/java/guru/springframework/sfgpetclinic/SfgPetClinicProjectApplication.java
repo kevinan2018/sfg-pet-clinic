b1585caf70498f1170b671b949d1e722cf31e7df
Kevin <kan@immersion.com>
2018-07-30 13:49:35 -0400

    sfg pet clinic init commit

---
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicProjectApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicProjectApplication.class, args);
+    }
+}
