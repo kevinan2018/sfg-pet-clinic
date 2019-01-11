bcd87b91805141d080298b6caf791b133c55b76d
Kevin <kan@immersion.com>
2019-01-11 15:51:14 -0500

    Added spring service config. Close #22.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetServiceMap.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetServiceMap.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 }
