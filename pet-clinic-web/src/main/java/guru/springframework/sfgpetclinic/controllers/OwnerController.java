7a114d04e9c9f5cee479c20c89e37572d7e8233b
Kevin <kan@immersion.com>
2019-01-12 16:32:51 -0500

    Fixed broken menu links. Close #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -16,9 +16,14 @@ public class OwnerController {
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ListOwners(Model model) {
+    public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping({"/find"})
+    public String findOwners() {
+        return "notimplemented";
     }
 }
