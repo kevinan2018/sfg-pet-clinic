1e3d107ced2f9c28785dbab5747c54af8cbf6da8
Kevin <kan@immersion.com>
2019-01-11 23:51:36 -0500

    Add Contact info Properties to Owner. Close #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephon;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephon() {
+        return telephon;
+    }
+
+    public void setTelephon(String telephon) {
+        this.telephon = telephon;
+    }
 
     public Set<Pet> getPets() {
         return pets;
