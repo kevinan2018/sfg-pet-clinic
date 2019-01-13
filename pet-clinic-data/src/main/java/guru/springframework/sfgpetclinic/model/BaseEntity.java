5dc69090b69ffb526b48aaf595df126d9d7999a4
Kevin <kan@immersion.com>
2019-01-13 12:41:52 -0500

    Update base entity with JPA annotations. Close #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public abstract class BaseEntity <T extends Comparable<? super T>> implements Serializable, Comparable<BaseEntity<T>> {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private T id;
 
     public T getId() {
