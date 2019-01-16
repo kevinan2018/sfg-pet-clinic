484beb86a5505e7dc441bc8c14483c61eafd009f
Kevin <kan@immersion.com>
2019-01-15 22:31:48 -0500

    Fixed Pet visit date validation failed issue. Close #67 Add ability to add or update a visit #57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ import java.time.LocalDate;
 public class Visit extends BaseEntity<Long>{
 
     @Column(name = "date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
     @Column(name = "description")
@@ -27,30 +29,6 @@ public class Visit extends BaseEntity<Long>{
     @ManyToOne
     @JoinColumn(name = "pet_id")
     private Pet pet;
-
-//    public LocalDate getDate() {
-//        return date;
-//    }
-//
-//    public void setDate(LocalDate date) {
-//        this.date = date;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public Pet getPet() {
-//        return pet;
-//    }
-//
-//    public void setPet(Pet pet) {
-//        this.pet = pet;
-//    }
 
     @Override
     public Long nextId(Long id) {
